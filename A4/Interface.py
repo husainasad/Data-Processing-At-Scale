7f8060ef5472564b6c22f3be328f7d6b4f1b8c30
husainasad <asad1999@gmail.com>
2023-10-30 21:09:11 -0700

    querying from range partitions

--- A4/Interface.py
+++ A4/Interface.py
@@ -23,6 +23,32 @@ def RangeQuery(ratingsTableName, ratingMinValue, ratingMaxValue, openconnection)
                 rr_data_list[i] = (rr_tableName,) + rr_data_list[i]
                 result_list.append(rr_data_list[i])
 
+    cur.execute('SELECT * FROM RangeRatingsMetadata')
+    range_partitions = cur.fetchall()
+    good_partitions = []
+    
+    if ratingMinValue==0:
+        good_partitions.append(0)
+    else:
+        for i in range(len(range_partitions)):
+            if ratingMinValue>range_partitions[i][1] and ratingMinValue<=range_partitions[i][2]:
+                good_partitions.append(i)
+                break
+
+    for i in range(len(range_partitions))[::-1]:
+        if ratingMaxValue>range_partitions[i][1] and ratingMaxValue<=range_partitions[i][2]:
+            good_partitions.append(i)
+            break
+
+    for i in range(good_partitions[0], good_partitions[1]+1):
+        range_tableName = 'RangeRatingsPart{0}'.format(i)
+        cur.execute('SELECT * FROM {0} WHERE Rating>={1} AND rating<={2}'.format(range_tableName, ratingMinValue, ratingMaxValue))
+        range_data_list = cur.fetchall()
+        if len(range_data_list)>0:
+            for i in range(len(range_data_list)):
+                range_data_list[i] = (range_tableName,) + range_data_list[i]
+                result_list.append(range_data_list[i])
+
     writeToFile('RangeQuery.txt', result_list)
 
 def PointQuery(ratingsTableName, ratingValue, openconnection):
@@ -40,6 +66,24 @@ def PointQuery(ratingsTableName, ratingValue, openconnection):
             for i in range(len(rr_data_list)):
                 rr_data_list[i] = (rr_tableName,) + rr_data_list[i]
                 result_list.append(rr_data_list[i])
+
+    cur.execute('SELECT * FROM RangeRatingsMetadata')
+    range_partitions = cur.fetchall()
+    good_partition = 0
+
+    if(ratingValue!=0):
+        for rp in range_partitions:
+            if(ratingValue>rp[1] and ratingValue<=rp[2]):
+                good_partition=rp[0]
+                break
+
+    range_tableName = 'RangeRatingsPart{0}'.format(good_partition)
+    cur.execute('SELECT * FROM {0} WHERE Rating={1}'.format(range_tableName, ratingValue))
+    range_data_list = cur.fetchall()
+    if len(range_data_list)>0:
+        for i in range(len(range_data_list)):
+            range_data_list[i] = (range_tableName,) + range_data_list[i]
+            result_list.append(range_data_list[i])
 
     writeToFile('PointQuery.txt', result_list)
 
